bf2ba7bb5c41fe4afafa62cd751feafb4decce6d
Rodrigo Arcos <rodrigo.arcos@usach.cl>
2019-09-08 01:38:01 -0300

    Representación y TDAs

---
+++ lab1_199949705_Arcos.rkt
@@ -0,0 +1,29 @@
+;TDA
+
+;----------------------------------------REPRESENTACION-----------------------------------------------------------
+;Los siguientes cuatro TDAs son representados en una lista, en la cual cada posicion de esta tiene las siguientes caracteristicas
+;1.- Enemigos
+;	->number identificador (0)
+;	->number numero del gusano
+;	->number x
+;	->number y
+
+;2.- Jugador
+;	->number identificador (1)
+;	->number numero del gusano
+;	->number x
+;	->number y
+
+;3.- Disparos
+;	->number identificador (2)
+;	->number angulo
+;	->number velocidad
+;	->number x
+;	->number y
+
+;4.- Obstaculos
+;	->number x
+;	->number y
+
+;Estos TDAs seran almacenados en una matriz (lista de listas), el cual sera la representacion del escenario.
+
